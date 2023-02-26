b2899d7b6ed9902aeacaf2a3b1c44070d56d7ec6
ScubaDrew615 <knoll-toothed-0e@icloud.com>
2023-02-26 17:34:41 -0600

    Added Constant

--- src/main/java/com/ab/constants/FrameworkConstants.java
+++ src/main/java/com/ab/constants/FrameworkConstants.java
@@ -8,7 +8,7 @@ public final class FrameworkConstants {
     private static final String CONFIGFILEPATH = System.getProperty("user.dir") + "/src/test/resources/config/FrameworkConfigs.properties";
 
     public static String getConfiguration() {
-    return CONFIGFILEPATH;
+        return CONFIGFILEPATH;
 
     }
 }
