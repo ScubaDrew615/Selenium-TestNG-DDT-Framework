7b0454ecec4a774949f5ec923dd9d712421f30b2
ScubaDrew615 <knoll-toothed-0e@icloud.com>
2023-02-28 03:03:13 -0600

    Created StringsUtil

---
+++ src/main/java/com/ab/utilities/StringsUtil.java
@@ -0,0 +1,17 @@
+package com.ab.utilities;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public final class StringsUtil {
+    private StringsUtil() {
+    }
+
+    public static String addSpacesAndCapitalize(String methodName) {
+        return Arrays.stream(methodName.split("(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])"))
+                .map(word -> Character.toUpperCase(word.charAt(0)) + word.substring(1))
+                .collect(Collectors.joining(" "));
+    }
+
+
+}
