66ad77aaa397436b1373532ed943f399bf9ed54e
ScubaDrew615 <knoll-toothed-0e@icloud.com>
2023-02-28 03:02:29 -0600

    Configured Extent Reports output destinations.

--- src/main/java/com/ab/constants/FrameworkConstants.java
+++ src/main/java/com/ab/constants/FrameworkConstants.java
@@ -1,12 +1,20 @@
 package com.ab.constants;
 
+import com.ab.enums.ConfigProperties;
+import com.ab.enums.DateFormats;
+import com.ab.utilities.DateUtil;
+import com.ab.utilities.PropertiesUtil;
+
 public final class FrameworkConstants {
 
     private FrameworkConstants() {
     }
 
     private static final String CONFIGFILEPATH = System.getProperty("user.dir") + "/src/test/resources/config/FrameworkConfigs.properties";
+    private static final String EXTENTREPORTSFOLDERPATH = System.getProperty("user.dir") + "/extent-test-output/";
+    private static String extentreportsfilepath = "";
     private static final int EXPLICITWAIT = 10;
+
 
     public static String getConfiguration() {
         return CONFIGFILEPATH;
@@ -15,5 +23,23 @@ public final class FrameworkConstants {
 
     public static int getExplicitWait() {
         return EXPLICITWAIT;
+    }
+
+    public static String getExtentReportsFilePath() throws Exception {
+        if (extentreportsfilepath.isEmpty()) {
+            extentreportsfilepath = createReportPath();
+        }
+        return extentreportsfilepath;
+    }
+
+    private static String createReportPath() throws Exception {
+        if (PropertiesUtil.getHashTableValues(ConfigProperties.OVERRIDEREPORTS).equalsIgnoreCase("no")) {
+            return EXTENTREPORTSFOLDERPATH + humanTime() + "/index.html";
+        } else {
+            return EXTENTREPORTSFOLDERPATH + "/index.html";
+        }
+    }
+    private static String humanTime(){
+        return DateUtil.convertUnixTime(System.currentTimeMillis(), DateFormats.LONG12H);
     }
 }
