5ba1e03136ef8e3c4c23015ce32ef7748663d7ee
ScubaDrew615 <knoll-toothed-0e@icloud.com>
2023-02-28 02:58:41 -0600

    Added ExtentLogger to BasePage

--- src/main/java/com/ab/pages/BasePage.java
+++ src/main/java/com/ab/pages/BasePage.java
@@ -3,18 +3,23 @@ package com.ab.pages;
 import com.ab.driver.DriverManager;
 import com.ab.enums.WaitStrategy;
 import com.ab.factories.ExplicitWaitFactory;
+import com.ab.reports.ExtentLogger;
 import org.openqa.selenium.By;
 
 public class BasePage {
 
-    protected void click(By locator, WaitStrategy strategy) {
+    protected void click(By locator, WaitStrategy strategy, String elementName) {
         ExplicitWaitFactory.explicitlyWait(WaitStrategy.CLICKABLE, locator).click();
+        ExtentLogger.pass(elementName + " is clicked");
 
 
     }
 
-    protected void sendKeys(By locator, String value, WaitStrategy strategy) {
-        ExplicitWaitFactory.explicitlyWait(WaitStrategy.PRESENCE, locator).sendKeys(value);
+    protected void sendKeys(By locator, String value, WaitStrategy strategy, String elementName) throws Exception {
+        ExplicitWaitFactory.explicitlyWait(WaitStrategy.PRESENCE, locator)
+                .sendKeys(value);
+        ExtentLogger.pass("(" + value + ") is entered successfully into \"" + elementName + "\"",true);
+
 
     }
 
