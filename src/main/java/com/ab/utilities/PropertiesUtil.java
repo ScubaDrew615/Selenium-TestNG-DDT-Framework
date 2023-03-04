74c844f8a84943e6515b5b72fe825415fbc92c72
ScubaDrew615 <knoll-toothed-0e@icloud.com>
2023-03-03 23:10:37 -0600

    Changed for loop logic

--- src/main/java/com/ab/utilities/PropertiesUtil.java
+++ src/main/java/com/ab/utilities/PropertiesUtil.java
@@ -9,22 +9,20 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 
-import static com.ab.utilities.StringsUtil.getCleanEnumName;
-
 public final class PropertiesUtil {
     private PropertiesUtil() {
     }
 
     private static Properties property = new Properties();
-    private static final Map<String, String> CONFIGMAP = new HashMap<>();
+    private static final Map<ConfigProperties, String> CONFIGMAP = new HashMap<>();
 
     static {
-        FileInputStream inputStream = null;
+        FileInputStream inputStream;
         try {
             inputStream = new FileInputStream(FrameworkConstants.getConfiguration());
             property.load(inputStream);
-            for (Map.Entry<Object, Object> entry : property.entrySet()) {
-                CONFIGMAP.put(String.valueOf(entry.getKey()), String.valueOf(entry.getValue()).trim());
+            for (ConfigProperties key : ConfigProperties.values()) {
+                CONFIGMAP.put(key, property.getProperty(key.name().toLowerCase()).trim());
             }
 
         } catch (Exception e) {
@@ -33,11 +31,11 @@ public final class PropertiesUtil {
     }
 
     public static String getValues(ConfigProperties key) throws Exception {
-        if (Objects.isNull(key) || Objects.isNull(CONFIGMAP.get(getCleanEnumName(key)))) {
-            throw new Exception("Property name '" + Objects.requireNonNull(key).name() + "' is not found. Please check FrameworkConfigs.properties.");
+        if (Objects.isNull(key) || Objects.isNull(CONFIGMAP.get(key))) {
+            if (Objects.nonNull(key)) {
+                throw new Exception("Property name '" + key.name() + "' is not found. Please check FrameworkConfigs.properties.");
+            }
         }
-        return CONFIGMAP.get(getCleanEnumName(key));
+        return CONFIGMAP.get(key);
     }
-
-
 }
