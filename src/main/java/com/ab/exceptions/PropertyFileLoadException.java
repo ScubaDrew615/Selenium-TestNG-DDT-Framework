403249e24803009e984d1dffcfb988a1b8823320
ScubaDrew615 <knoll-toothed-0e@icloud.com>
2023-03-06 00:49:09 -0600

    Deleted PropertyFileLoadException

--- src/main/java/com/ab/exceptions/PropertyFileLoadException.java
+++
@@ -1,7 +0,0 @@
-package com.ab.exceptions;
-
-public class PropertyFileLoadException extends FrameworkExceptions{
-    public PropertyFileLoadException(String message) {
-        super(message);
-    }
-}
