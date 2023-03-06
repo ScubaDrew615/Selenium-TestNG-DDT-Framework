3c1baa3cf594501cbc8ad4899d2e8abdb33c3e4c
ScubaDrew615 <knoll-toothed-0e@icloud.com>
2023-03-06 00:16:46 -0600

    Added custom exceptions

---
+++ src/main/java/com/ab/exceptions/PropertyNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ab.exceptions;
+
+public class PropertyNotFoundException extends FrameworkExceptions{
+    public PropertyNotFoundException(String message) {
+        super(message);
+    }
+}
