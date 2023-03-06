de8e874c9bc20c015dbe5cec272810a8f0770781
ScubaDrew615 <knoll-toothed-0e@icloud.com>
2023-03-05 20:29:56 -0600

    Made retires local

--- src/main/java/com/ab/listeners/RetryFailedTests.java
+++ src/main/java/com/ab/listeners/RetryFailedTests.java
@@ -8,14 +8,14 @@ import org.testng.ITestResult;
 public class RetryFailedTests implements IRetryAnalyzer {
 
     private int count = 0;
-    private final int RETRIES = 1;
 
     @Override
     public boolean retry(ITestResult iTestResult) {
         boolean value = false;
         try {
             if (PropertiesUtil.getValues(ConfigProperties.RETRY_FAILED_TESTS).equalsIgnoreCase("yes")) {
-                value = count < RETRIES;
+                int retries = 1;
+                value = count < retries;
                 count++;
             }
         } catch (Exception e) {
