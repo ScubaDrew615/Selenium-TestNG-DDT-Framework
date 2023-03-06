45fcb0b7138af8f952367847b326eae57fc1e4b5
ScubaDrew615 <knoll-toothed-0e@icloud.com>
2023-03-05 20:27:08 -0600

    Suppressed warnings

--- src/main/java/com/ab/utilities/DataProviderUtil.java
+++ src/main/java/com/ab/utilities/DataProviderUtil.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@SuppressWarnings("DataProviderReturnType")
 public final class DataProviderUtil {
     private static List<Map<String, String>> dataList = new ArrayList<>();
 
